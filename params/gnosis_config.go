701d36815299bdd78764a63ae218b920f355e385
SamAg19 <sameermg1999@gmail.com>
2025-05-02 11:46:21 +0530

    updated metalayer alloc

--- params/gnosis_config.go
+++ params/gnosis_config.go
@@ -29,11 +29,11 @@ func readChainSpec(filename string) *ChainConfig {
 var (
 	GnosisGenesisHash    = common.HexToHash("0x4f1dd23188aab3a76b463e4af801b52b1248ef073c648cbdc4c9333d3da79756")
 	ChiadoGenesisHash    = common.HexToHash("0xada44fd8d2ecab8b08f256af07ad3e777f17fb434f8f8e678b312f576212ba9a")
-	MetalayerGenesisHash = common.HexToHash("0x6367b754dabd79721c97653beeb1a63d8b65cb6e30637d1bb6dd63ef09130dac")
+	MetalayerGenesisHash = common.HexToHash("0x56d44e60411eae7f4f4544f5c1cb70178dfb37e0434f6573e5cd138101d906ab")
 
 	GnosisGenesisStateRoot    = common.HexToHash("0x40cf4430ecaa733787d1a65154a3b9efb560c95d9e324a23b97f0609b539133b")
 	ChiadoGenesisStateRoot    = common.HexToHash("0x9ec3eaf4e6188dfbdd6ade76eaa88289b57c63c9a2cde8d35291d5a29e143d31")
-	MetalayerGenesisStateRoot = common.HexToHash("0x6225caad148323b95c917a16f5ccf766a39e4beea91d226a36b00ad549a972b5")
+	MetalayerGenesisStateRoot = common.HexToHash("0xda8db002b6a9dade245b8269545a86b51f96cee2579b8fd394adf18c2ca7ec99")
 
 	GnosisChainConfig    = readChainSpec("chainspecs/gnosis.json")
 	ChiadoChainConfig    = readChainSpec("chainspecs/chiado.json")
